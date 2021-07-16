83880ab3e9bc098504fd8513f2cd86a92d6cf786
Sarah Gu <guwen888@gmail.com>
2021-07-16 13:51:25 -0400

    added blank template line graph from Charts API, not populated with any data

---
+++ myAIScribe/ViewControllers/GraphViewController.h
@@ -0,0 +1,16 @@
+//
+//  GraphViewController.h
+//  myAIScribe
+//
+//  Created by Sarah Wen Gu on 7/16/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GraphViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ myAIScribe/ViewControllers/GraphViewController.m
@@ -0,0 +1,84 @@
+//
+//  GraphViewController.m
+//  myAIScribe
+//
+//  Created by Sarah Wen Gu on 7/16/21.
+//
+
+#import "GraphViewController.h"
+@import Charts;
+
+@interface GraphViewController () <ChartViewDelegate>
+@property (weak, nonatomic) IBOutlet LineChartView *chartView;
+
+@end
+
+@implementation GraphViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    _chartView.delegate = self;
+    
+    
+    _chartView.chartDescription.enabled = NO;
+    
+    _chartView.dragEnabled = YES;
+    [_chartView setScaleEnabled:YES];
+    _chartView.pinchZoomEnabled = YES;
+    _chartView.drawGridBackgroundEnabled = NO;
+
+    // x-axis limit line
+    ChartLimitLine *llXAxis = [[ChartLimitLine alloc] initWithLimit:10.0 label:@"Index 10"];
+    llXAxis.lineWidth = 4.0;
+    llXAxis.lineDashLengths = @[@(10.f), @(10.f), @(0.f)];
+    llXAxis.labelPosition = ChartLimitLabelPositionBottomRight;
+    llXAxis.valueFont = [UIFont systemFontOfSize:10.f];
+    
+    //[_chartView.xAxis addLimitLine:llXAxis];
+    
+    _chartView.xAxis.gridLineDashLengths = @[@10.0, @10.0];
+    _chartView.xAxis.gridLineDashPhase = 0.f;
+    
+    ChartLimitLine *ll1 = [[ChartLimitLine alloc] initWithLimit:150.0 label:@"Upper Limit"];
+    ll1.lineWidth = 4.0;
+    ll1.lineDashLengths = @[@5.f, @5.f];
+    ll1.labelPosition = ChartLimitLabelPositionTopRight;
+    ll1.valueFont = [UIFont systemFontOfSize:10.0];
+    
+    ChartLimitLine *ll2 = [[ChartLimitLine alloc] initWithLimit:-30.0 label:@"Lower Limit"];
+    ll2.lineWidth = 4.0;
+    ll2.lineDashLengths = @[@5.f, @5.f];
+    ll2.labelPosition = ChartLimitLabelPositionBottomRight;
+    ll2.valueFont = [UIFont systemFontOfSize:10.0];
+    
+    ChartYAxis *leftAxis = _chartView.leftAxis;
+    [leftAxis removeAllLimitLines];
+    [leftAxis addLimitLine:ll1];
+    [leftAxis addLimitLine:ll2];
+    leftAxis.axisMaximum = 200.0;
+    leftAxis.axisMinimum = -50.0;
+    leftAxis.gridLineDashLengths = @[@5.f, @5.f];
+    leftAxis.drawZeroLineEnabled = NO;
+    leftAxis.drawLimitLinesBehindDataEnabled = YES;
+    
+    _chartView.rightAxis.enabled = NO;
+    
+    //[_chartView.viewPortHandler setMaximumScaleY: 2.f];
+    //[_chartView.viewPortHandler setMaximumScaleX: 2.f];
+
+    _chartView.legend.form = ChartLegendFormLine;
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
